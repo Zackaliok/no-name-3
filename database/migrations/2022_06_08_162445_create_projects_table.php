dd2ce381d54051bb38aa1bea20ca2650177f77e9
Tristan Fumiere <zackaliok.contact@gmail.com>
2022-06-09 01:16:01 +0200

    first commit

---
+++ database/migrations/2022_06_08_162445_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->longtext('description');
+            $table->text('github_link');
+            $table->text('image_link');
+            $table->text('project_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
